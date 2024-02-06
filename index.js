7c883ac134e51f1ef23d4a1fe335d800373f0fd9
HanaaMAldaly <hanaaaldaly@gmail.com>
2024-02-06 13:29:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
+import * as core from '@actions/core' 
+  await core.summary
+  .addHeading('Test Results')
+  .addCodeBlock(generateTestResults(), "js")
+  .addTable([
+    [{data: 'File', header: true}, {data: 'Result', header: true}],
+    ['foo.js', 'Pass ✅'],
+    ['bar.js', 'Fail ❌'],
+    ['test.js', 'Pass ✅']
+  ])
+  .addLink('View staging deployment!', 'https://github.com')
+  .write()
+
 try{
     const name = core.getInput('who-to-greet')
     console.log(`Hello ${name}!`)
