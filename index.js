1c196a79f7af739b5f71f0f8d6716f1171d5a9b6
HanaaMAldaly <hanaaaldaly@gmail.com>
2024-02-06 14:20:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
+ await core.summary
+  .addHeading('Test Results')
+  .addLink('View staging deployment!', 'https://github.com')
+  .write()
+
 try{
     const name = core.getInput('who-to-greet')
     console.log(`Hello ${name}!`)
@@ -9,10 +14,6 @@ try{
     const payload = JSON.stringify(github.context.payload,undefined,2)
     console.log(`The event paylod: ${payload}`)
   
-   await core.summary
-  .addHeading('Test Results')
-  .addLink('View staging deployment!', 'https://github.com')
-  .write()
 }
 catch(error){
     core.setFailed(error.message)
