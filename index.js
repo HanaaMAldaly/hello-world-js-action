b992bf56c5ef9d980295da0f8a6df20b4ad148ab
hanaa.ahmed1 <hanaa.ahmed1@vodafone.com>
2024-02-06 23:20:01 +0200

    format

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ const prList =[{title: 'one',number:1,link:"wwww",owner:"hanaa"},
 //`
   releaseNotes.map((pr)=>{
 
-   myMarkdown = myMarkdown.concat(`| #[${pr.number}](${pr.link}) | ${pr.title} | ${pr.ticketsString} | ${pr.owner} | `)
+   myMarkdown = myMarkdown.concat(`| #(${pr.link}) | ${pr.title} | ${pr.ticketsString} | ${pr.owner} | `)
   })
 //  core.summary
 //  .addHeading(releaseNotes[0],'2')
