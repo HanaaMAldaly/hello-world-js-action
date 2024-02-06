fa955bc0712cad49d1182cecf17881d9e7620d20
hanaa.ahmed1 <hanaa.ahmed1@vodafone.com>
2024-02-06 23:05:49 +0200

    format

--- index.js
+++ index.js
@@ -93,7 +93,13 @@ const prList =[{title: 'one',number:1,link:"wwww",owner:"hanaa"},
   link:'[3117](https://github.vodafone.com/VFGroup-MyVodafone-OnePlatform/MyVodafone-OneApp/pull/3117',
   owner: 'marwan-salem',
   ticketsString:'OPB-16885'
-  }]
+  },
+  {title:'fix: CMSUnderAgeContract path  CMSUnderAgeContract path  CMSUnderAgeContract path  CMSUnderAgeContract path  CMSUnderAgeContract path  CMSUnderAgeContract path',
+    number:3179,
+    link:'https://github.vodafone.com/VFGroup-MyVodafone-OnePlatform/MyVodafone-OneApp/pull/3117',
+    owner: 'marwan-salem',
+    ticketsString:'OPB-16885'
+    }]
   //(#[3179](https://github.vodafone.com/VFGroup-MyVodafone-OnePlatform/MyVodafone-OneApp/pull/3179)) ([OPB-16885](https://cps.jira.agile.vodafone.com/browse/OPB-16885)) - by marwan-salem (05-02-2024 16:25 +00:00)',
 //  '- feat: Add general error boundary  (#[3104](https://github.vodafone.com/VFGroup-MyVodafone-OnePlatform/MyVodafone-OneApp/pull/3104)) ([OPB-17039](https://cps.jira.agile.vodafone.com/browse/OPB-17039)) - by hassan-mostafa2 (05-02-2024 16:22 +00:00)',
 //  '- fix: security settings app id issue (#[3150](https://github.vodafone.com/VFGroup-MyVodafone-OnePlatform/MyVodafone-OneApp/pull/3150)) ([OPB-18072](https://cps.jira.agile.vodafone.com/browse/OPB-18072)) - by iman-hegazy (05-02-2024 16:15 +00:00)',
@@ -124,7 +130,7 @@ const prList =[{title: 'one',number:1,link:"wwww",owner:"hanaa"},
 //     |--- |--- | --- | --- |
 //`
   releaseNotes.map((pr)=>{
-   myMarkdown = myMarkdown.concat(`| ${pr.number} | ${pr.title} | ${pr.ticketsString} | ${pr.owner} | `)
+   myMarkdown = myMarkdown.concat(`| [${pr.number}](${pr.link}) | ${pr.title} | ${pr.ticketsString} | ${pr.owner} | `)
   })
 //  core.summary
 //  .addHeading(releaseNotes[0],'2')
